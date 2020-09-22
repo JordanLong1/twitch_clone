e8f39921415d476655b743239b50564a8ed46d70
Jordan <jordanleelong5@gmail.com>
2020-09-22 13:19:36 -0700

    imported Field & reduxForm and got the fields working

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,10 +1,50 @@
 import React from 'react'; 
+import { Field, reduxForm} from 'redux-form' // F is capitalized because it is a react component, reduxForm is lowercase becausse it is a function
 
+class StreamCreate extends React.Component {
 
-const StreamCreate = () => {
-    return (
-        <div>Stream Create</div>
-    )
+    renderInput( {input, label}) {
+        return (
+            <div className='field'>
+                <label>{label}</label>
+                <input {...input}/>
+            </div>
+
+        ) 
+    }
+
+    onSubmit = formValues => {
+    console.log(formValues)
+    }
+
+    render() {
+        return (
+          <form className='ui form' onSubmit={this.props.handleSubmit(this.onSubmit)}>
+              <Field name='title' component={this.renderInput} label='Enter Title' />
+              <Field name='description' component={this.renderInput} label='Enter Description'/>
+          <button className='ui button primary'>
+            Submit
+          </button>
+          </form>
+        )
+    }
 }
 
-export default StreamCreate;
+const validate = (formValues) => {
+    const errors = {}; 
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title'
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description'
+    }
+
+    return errors
+}
+
+export default reduxForm({
+    form: 'streamCreate', 
+    validate
+})(StreamCreate);
