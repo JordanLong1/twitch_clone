2fb4ff2c1971d2c9aa5439834630263040b3032c
Jordan <jordanleelong5@gmail.com>
2020-09-24 00:26:15 -0700

    imported types for actions and created all actions needed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/Streams'
-import {SIGN_IN, SIGN_OUT, CREATE_STREAM} from './types'
+import {
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM,
+     FETCH_STREAMS, 
+     FETCH_STREAM, 
+     DELETE_STREAM, 
+     EDIT_STREAM
+} from './types'
 
 export const signIn = (userId) => {
     return {
@@ -18,4 +26,27 @@ export const createStream = (formValues) => {
        const response = await streams.post('/streams', formValues); // post request with axios
         dispatch({ type: CREATE_STREAM, payload: response.data})
     }
+}
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams'); 
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data})
+}
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({ type: FETCH_STREAM, payload: response.data})
+}
+
+export const editStream = (id, formvalues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formvalues) // we use form values cause we need to specify what it is we are updating
+
+    dispatch({ type: EDIT_STREAM, payload: response.data});
+}
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`); 
+
+    dispatch({ type: DELETE_STREAM, payload: id}); 
 }
