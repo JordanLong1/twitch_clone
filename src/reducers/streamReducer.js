cd479f18419b68154eeef31388bf1bbf59e39f57
Jordan <jordanleelong5@gmail.com>
2020-09-24 14:05:05 -0700

    added fetch streams and delete streams

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import {
     FETCH_STREAMS, 
     FETCH_STREAM, 
@@ -8,13 +9,18 @@ import {
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+            return {...state, ..._.mapKeys(action.payload, 'id')} // creating a new object, taking all the current records inside of our state object and adding them in
+            // then calling mapKeys and take the list of streams from the api and going to create an object out of it using mapKeys with the key of 'id'
         case FETCH_STREAM:
             return {...state, [action.payload.id]: action.payload}; // [] in this scenario is key interpolation es2015 syntax
             case CREATE_STREAM:
                 return {...state, [action.payload.id]: action.payload};
                 case EDIT_STREAM:
                     return { ...state, [action.payload.id]: action.payload };
-                    
+            case DELETE_STREAM:
+                return _.omit(state, action.payload) // we dont need to pass in id here because the payload is the id in the action creator
+                // omit is not going to mutate state
         default: 
         return state; 
     }
