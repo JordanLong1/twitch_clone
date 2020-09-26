c2a2f9e22b7eaa92577acab125afaf72235b4087
Jordan <jordanleelong5@gmail.com>
2020-09-25 21:50:48 -0700

    imported fetchStream, addded ownProps onto maptostate

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,12 +1,26 @@
 import React from 'react'; 
+import {connect} from 'react-redux'
+import {fetchStream} from '../../actions'
+class StreamEdit extends React.Component {
 
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id); 
+    }
 
-const StreamEdit = () => {
-    return (
-        <div>
-           StreamEdit
-        </div>
-    )
+    render() {
+        if (!this.props.stream) {
+            return <div>...Loading</div>
+        }
+        return (
+            <div>
+           {this.props.stream.title}
+            </div>
+        )
+    }
 }
 
-export default StreamEdit;
+const mapStateToprops = (state, ownProps) => { // ownProps is the reference to the props object that shows up inside of this component
+    return {stream: state.streams[ownProps.match.params.id]  } // this gives us the stream id from this components props via ownProps
+}
+
+export default connect(mapStateToprops, {fetchStream})(StreamEdit);
