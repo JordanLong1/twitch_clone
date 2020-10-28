67bce08ea70a146137ccb2d9498ed31852035b52
Jordan <jordanleelong5@gmail.com>
2020-10-28 15:34:04 -0700

    added componentDidMount() and renderContent helper method

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React from 'react'; 
+import {connect} from 'react-redux'
 import Modal from '../Modal'
 import history from '../../history'
+import {fetchStream} from '../../actions'
 class StreamDelete extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
     renderActions() {
     return (
@@ -12,19 +18,30 @@ class StreamDelete extends React.Component {
        </React.Fragment>  
     )
     }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return "Are you sure you want to delete this stream?"
+        }
+
+        return `Are you sure you want to delete this stream with title: ${this.props.stream.title} `
+    }
     render() {
     return (
-        <div>
-           StreamDelete
+        
            <Modal 
            title={"Delete Stream"}
-           content={"Are you sure you want to delete this stream?"}
+           content={this.renderContent()}
            actions={this.renderActions()}
            onDismiss={ () => history.push('/')}
            />
-        </div>
+    
     );
     }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]}
+}; 
+
+export default connect(mapStateToProps, {fetchStream})(StreamDelete);
