f51cd77cdd01751b329115122be5f5bc76d243a0
Jordan <jordanleelong5@gmail.com>
2020-11-01 23:37:35 -0800

    imported switch so streams/show wouldnt show on streams/new

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 // with react-router each component needs to be designed to work in isolation (fetch its own data)
 import StreamCreate from './streams/StreamCreate'
 import StreamEdit from './streams/StreamEdit'
@@ -16,11 +16,13 @@ const App = () => {
   
             <div>
             <Header />
+            <Switch >
             <Route path='/' exact component={StreamList} />
             <Route path='/streams/new' exact component={StreamCreate}/>
             <Route path='/streams/edit/:id' exact component={StreamEdit}/> 
             <Route path='/streams/delete/:id' exact component={StreamDelete} />
-            <Route path='/streams/:id' exact component={StreamShow}/> 
+            <Route path='/streams/:id' exact component={StreamShow}/>
+            </Switch> 
             </div>
 
            
